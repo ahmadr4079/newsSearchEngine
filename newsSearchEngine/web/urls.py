4313ec94fa9ad3e440d11866a644e54499bfd7f0
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-29 19:56:43 +0430

    add web url to base url and create web url

--- newsSearchEngine/web/urls.py
+++ newsSearchEngine/web/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('web.urls'))
+    path('home',index,name='index')
 ]
