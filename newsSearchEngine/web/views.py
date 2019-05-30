4bff9f5b1c0b00e851f816a529dec753410c81bb
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-30 14:58:02 +0430

    get user query and handle if query is empty return message to user

--- newsSearchEngine/web/views.py
+++ newsSearchEngine/web/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 
+@csrf_protect
 def index(request):
-    return render(request,'mainPage/index.html')
+    if request.method == 'POST':
+        query = request.POST['inputQuery']
+        if query == '':
+            context = {
+                'message' : 'لطفا عبارت مورد نظر خود را وارد کنید'
+            }
+            return render(request,'mainPage/index.html',context=context)
+        else:
+            return render(request,'mainPage/index.html')
+    else:
+        return render(request,'mainPage/index.html')
