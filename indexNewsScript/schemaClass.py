c6c5259008876c8f93fe6af3d74fc7218561753f
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-31 17:29:55 +0430

    create schema for index data

---
+++ indexNewsScript/schemaClass.py
@@ -0,0 +1,10 @@
+from whoosh.fields import SchemaClass,TEXT,DATETIME,ID
+
+class NewsSchema(SchemaClass):
+    newsAgency = TEXT
+    title = TEXT(stored=True)
+    url = ID(stored=True)
+    content = TEXT
+    # date = DATETIME
+    # time = DATETIME
+    
