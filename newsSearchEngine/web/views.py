cac7f3242d5394837870127e3d432d516fcacd5e
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-05-31 09:53:14 +0430

    new design for search page and put search from in it

--- newsSearchEngine/web/views.py
+++ newsSearchEngine/web/views.py
@@ -7,19 +7,39 @@ from .indexNews import IndexNews
 
 # Create your views here.
 
+@csrf_protect
 def search(request):
-    inputQuery = request.session['inputQuery']
-    inputQuery = inputQuery
+    print('you are in search function')
     indexNewsObject = IndexNews()
     ix = indexNewsObject.ix
-    queryParser = QueryParser(fieldname='content',schema=ix.schema,group=OrGroup)
-    query = queryParser.parse(inputQuery)
-    with ix.searcher() as searcher:
-        results = searcher.search(query,terms=True)
-        context = {
-        'results':results
-        }
-        return render(request,'searchPage/searchPage.html',context=context)
+    if request.method == 'POST':
+        print('request is post')
+        inputQuery = request.POST['inputQuerySearchPage']
+        if inputQuery == '':
+            context = {
+                'message' : 'لطفا عبارت مورد نظر خود را وارد کنید'
+            }
+            return render(request,'searchPage/searchPage.html',context=context)
+        else:
+            queryParser = QueryParser(fieldname='content',schema=ix.schema,group=OrGroup)
+            query = queryParser.parse(inputQuery)
+            with ix.searcher() as searcher:
+                results = searcher.search(query,terms=True)
+                context = {
+                'results':results
+                }
+                return render(request,'searchPage/searchPage.html',context=context)
+    else:
+        inputQuery = request.session['inputQuery']
+        inputQuery = inputQuery
+        queryParser = QueryParser(fieldname='content',schema=ix.schema,group=OrGroup)
+        query = queryParser.parse(inputQuery)
+        with ix.searcher() as searcher:
+            results = searcher.search(query,terms=True)
+            context = {
+            'results':results
+            }
+            return render(request,'searchPage/searchPage.html',context=context)
 
 
 @csrf_protect
